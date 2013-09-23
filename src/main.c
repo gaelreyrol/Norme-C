68a5f6602e8793e9111535619c22ee6230198771
Zevran <info@zevran.net>
2013-09-23 02:13:03 +0200

    Fix typo

--- src/main.c
+++ src/main.c
@@ -14,49 +14,48 @@
 
 void    scan(char *file_name)
 {
-    t_file  *file;
-    t_list_content *t = malloc(sizeof(t_list_content));
-    t_reason *reason = NULL;
+	t_file  *file;
+	t_list_content *t = malloc(sizeof(t_list_content));
+	t_reason *reason = NULL;
 
-    file->name = file_name;
-    init_content_list(t);
-    get_extension(file, &reason);
-    if (file->is_valid)
-    {
-        init_content_list(t);
-        if (ft_open_file(file, t))
-        {
-            ft_print_name(file->name);
-            view_content_list(t);
-        }
-    }
-    else
-    {
-        view_reason_list(file, reason);
-        clear_reason_list(&reason);
-        return ;
-    }
+	file->name = file_name;
+	init_content_list(t);
+	get_extension(file, &reason);
+	if (file->is_valid)
+	{
+		if (ft_open_file(file, t))
+		{
+			ft_print_name(file->name);
+			view_content_list(t);
+		}
+	}
+	else
+	{
+		view_reason_list(file, reason);
+		clear_reason_list(&reason);
+		return ;
+	}
 }
 
-void    help(void)
+void	help(void)
 {
-    ft_putstr("Usage : file_name.(c/h)\n");
+	ft_putstr("Usage : file_name.(c/h)\n");
 }
 
 int main(int argc, char **argv)
 {
-    int i;
-    
-    i = 1;
-    if (argc == 1)
-        help();
-    else
-    {
-        while (i < argc)
-        {
-            scan(argv[i]);
-            i++;
-        }
-    }
-    return (0);
+	int i;
+
+	i = 1;
+	if (argc == 1)
+		help();
+	else
+	{
+		while (i < argc)
+		{
+			scan(argv[i]);
+			i++;
+		}
+	}
+	return (0);
 }
